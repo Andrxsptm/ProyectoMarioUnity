2f115f13867f663499249de45dfc7dab6e261d0d
andrxsdev <andrxs150@gmail.com>
2026-04-15 10:31:03 -0500

    Cambio de escenas

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,22 +1,16 @@
-
 using TMPro;
 using UnityEngine;
 
-public class GameEngine : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
-    public static GameEngine Instance;
+    public static UIManager Instance;
     public TMP_Text monedasText;
-    public int moneda;
-    public int vidas;
- 
 
     private void Awake()
     {
         if (Instance == null)
         {
-            DontDestroyOnLoad(gameObject);
             Instance = this;
-            vidas = 10;
         }
         else
         {
@@ -26,25 +20,12 @@ public class GameEngine : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    public void SumarMonedas()
-    {
-        moneda++;
-        monedasText.text = "Monedas: " + moneda;
-    }
-    public void QuitarVidas()
-    {
-
-        vidas--;
-        Debug.Log(vidas);
+        
     }
 }
-
